7573c0ea288af365b7ea753000255fc9772bc4fc
MaZderMind <github@mazdermind.de>
2014-06-02 23:52:04 +0200

    documentation and comments

---
+++ README.md
@@ -0,0 +1,12 @@
+Frab-Based Release-Form-Generator
+=================================
+
+This script will generate you a pdf for speakers to sign of that their talk will be stremed, recorded and published. It generates a staple of pages for each day & room to be laid out in the rooms.
+
+Usage
+-----
+Install the required gems, then call it with your Frab/Pentabarf compatible xml and save the pdf:
+
+	bundle
+	./generate-form.rb http://sotm-eu.org/export.xml > sotm-eu-form.pdf
+
--- generate-form.rb
+++ generate-form.rb
@@ -1,25 +1,37 @@
 #!/usr/bin/ruby
 # encoding: UTF-8
 
+# http reader
 require 'open-uri'
+
+# xml parser
 require 'nokogiri'
+
+# pdf formatter
 require 'prawn'
 
+
+
+# test for required argument
 if ARGV.length < 1 then
 	$stderr.puts "call with schedule-xml url and pipe output to a pdf-file like this:"
 	$stderr.puts "  ./generate-form.rb http://sotm-eu.org/export.xml > sotm-eu-form.pdf"
 	exit
 end
 
+# extract the url, download and open it
 url = ARGV[0]
 open(url) do |f|
+	# parse the resulting xml
 	xml = Nokogiri::XML(f)
-	
+
+	# create a new A4-Landscape sized pdf-document
 	pdf = Prawn::Document.new(
 		:page_size => "A4",
 		:page_layout => :landscape
 	)
 
+	# extract the event-info from the header of the schedule-xml
 	eventinfo = {}
 	xml.xpath('/schedule/conference').tap do |conference|
 		eventinfo = {
@@ -30,59 +42,104 @@ open(url) do |f|
 		}
 	end
 
+	# avoid page-break on the first page
 	first_page = true
-	xml.xpath('/schedule/day').map do |day|
 
+	# iterate through all days and all rooms
+	xml.xpath('/schedule/day').map do |day|
 		day.xpath('./room').map do |room|
+			# add a page-break unless we're on the first page
 			pdf.start_new_page unless first_page
 			first_page = false
+
+			# record the page-number where that room starts
 			start_page = pdf.page_number
+
+			# set default font-size
 			pdf.font_size(14)
 
+			# start the table with its header
 			tablecontent = [
 				['#', 'Time', 'Speaker', 'Title', 'Signature']
 			]
 
+			# iterate through all events
 			room.xpath('./event').map do |event|
+				# push a row to that table
 				tablecontent.push([
 					event['id'],
 					event.xpath('start').text,
+
+					# concatanate person-names with a comma
 					event.xpath('persons/person').map { |person| person.text }.join(', '),
 					event.xpath('title').text,
-					{:content => "", :borders => [:top, :bottom, :left]}
+
+					# empty cell for the signature
+					""
 				])
 			end
 
+			# define a bounding-box that will leave room (140 points) at the top of the page for
+			# the text-header. that bounding box will reserve those 140 points on each page the
+			# table flows onto, so on each page there will be 140 points free space
 			pdf.bounding_box([0, pdf.bounds.top - 140], :width => pdf.bounds.width) do
+				# paint the table into this bounding-box
 				pdf.table(
 					tablecontent,
+
+					# repeat the first row on each page
 					:header => true,
+
+					# specify column width in points
 					:column_widths => [30, 50, 150, 339, 200]
 				) do |table|
+					# post-parse-pre-layout formatting rules
+
+					# force cell-height and padding
 					table.cells.height = 60
 					table.cells.padding = [10, 5]
 
+					# specify borders for the normal cells and for the signature cell
 					table.columns(0..3).borders = [:top, :bottom]
 					table.columns(4).borders = [:top, :bottom, :left]
 
+					# format the header-row as bold
 					table.rows(0).font_style = :bold
 				end
 			end
 
+			# record last page the table flew onto
 			end_page = pdf.page_number
-			n_pages = end_page - start_page + 1
+
+			# add a repeater for all the pages the table for this room has filled
+			# :dynamic makes prawn reevaluate the repeated block on each page, allowing us to do
+			# page-number calculations
 			pdf.repeat(start_page..end_page, :dynamic => true) do
+				# move to the top of that page
 				pdf.move_cursor_to pdf.bounds.top
+
+				# big title
 				pdf.font_size(25) do
 					pdf.text eventinfo[:title]+" – Video Release Form"
 				end
 
+				# not so big subtitle
 				pdf.font_size(20) do
-					pdf.text 'Day %d <i>(%s)</i>, %s – Page %d of %d' % 
-						[day['index'], day['date'], room['name'], (pdf.page_number - start_page + 1), n_pages],
-						:inline_format => true
+					pdf.text 'Day %d <i>(%s)</i>, %s – Page %d of %d' % [
+						day['index'],
+						day['date'],
+						room['name'],
+						
+						# page-index in the set of pages the per-room-and-day-table filled
+						# in other words: page-number per room and day
+						(pdf.page_number - start_page + 1),
+
+						# number of pages the table required
+						end_page - start_page + 1
+					], :inline_format => true
 				end
 
+				# acknowledgement text with a little extra padding
 				pdf.pad_top(15) do
 					pdf.text \
 						"I/We, the undersigned, agree that <b>video footage of my talk</b> at the "+eventinfo[:title]+" "+
@@ -94,6 +151,6 @@ open(url) do |f|
 		end
 	end
 
+	# render to stdout
 	puts pdf.render
-
 end
